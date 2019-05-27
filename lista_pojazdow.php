4150a33a5032ee61557b294c10ac1a986fb28452
Kokos12345 <50366939+Kokos12345@users.noreply.github.com>
2019-05-27 22:12:35 +0200

    Create lista_pojazdow.php

---
+++ lista_pojazdow.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+
+<title>Plutex::Lista pojazdów</title>
+</head>
+
+<body>
+<div class="container">
+<div class="bar"><h1>Lista pojazdów</h1></div>
+
+<table>
+<tr>
+<th>Marka</th>
+<th>Model</th>
+<th>Dostępność</th>
+</tr>
+<?php>
+$conn = mysqli_connect("localhost" , "root", "", "kokos");
+$sql = "SELECT Marka, Model, Dostępność from cars"
+$result = $conn->query($sql);
+if ($result-> num_rows > 0) {
+	while ($row = $result-> fetch_assoc()) {
+		echo "<tr><td>". $row["id"]."</td><td>". $row ["Marka"] . "</td><td>" . $row ["Model"] . "</td><td>" . $row ["Dostępność"] . "</td></tr>";
+	}
+		echo "</table";
+  } 
+  else 
+  {
+  echo "0 result";
+  }
+  $conn -> close();
+  ?>
+		</table>
+
+</div>
+</body>
+
+</html> 
